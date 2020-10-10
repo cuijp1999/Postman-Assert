92f7136f0d9aa90d421ba2b5447bb5f4e7ea69f2
lzl <benbenxiong567@163.com>
2020-10-10 13:23:23 +0800

    init

--- 常用断言.js
+++ 常用断言.js
@@ -40,3 +40,17 @@ pm.test("长度区间", function () {
     pm.expect(pm.response.json().name).to.have.lengthOf.within(3,5);
 });
 
+/*
+js常用类型判断
+
+表达式						返回值
+typeof undefined			'undefined'
+typeof null					'object'
+typeof true					'boolean'
+typeof 123					'number'
+typeof "abc"				'string'
+typeof function() {}		'function'
+typeof {}					'object'
+typeof []					'object'
+typeof unknownVariable		'undefined'
+*/

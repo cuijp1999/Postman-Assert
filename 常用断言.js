7d477f4e59c44ec9b9cf0046c236e01a87401fd1
lzl <benbenxiong567@163.com>
2020-10-10 13:19:29 +0800

    init

---
+++ 常用断言.js
@@ -0,0 +1,42 @@
+
+pm.test("断言状态码", function () {
+    pm.response.to.have.status(200);
+});
+
+
+pm.test("响应体包含字符串", function () {
+    pm.expect(pm.response.text()).to.include("string_you_want_to_search");
+});
+
+pm.test("json串取值断言", function () {
+    var jsonData = pm.response.json();
+    pm.expect(jsonData.value).to.eql(100);
+});
+
+
+
+// 类型断言
+pm.test("Your test name", function () {
+    var jsonData = pm.response.json();
+    pm.expect(typeof jsonData.items[0].key_word).to.eql('string');
+});
+
+
+
+// 长度断言
+pm.test("等于长度", function () {
+    pm.expect(pm.response.json().name).to.have.lengthOf(4);
+});
+
+pm.test("长度大于", function () {
+    pm.expect(pm.response.json().name).to.have.lengthOf.above(3);
+});
+
+pm.test("长度小于", function () {
+    pm.expect(pm.response.json().name).to.have.lengthOf.below(5);
+});
+
+pm.test("长度区间", function () {
+    pm.expect(pm.response.json().name).to.have.lengthOf.within(3,5);
+});
+
